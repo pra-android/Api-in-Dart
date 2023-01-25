96d2643986c1805b5feae0557fd06673ce47b0df
Prabin Bhattarai <82046769+pra-android@users.noreply.github.com>
2023-01-25 16:27:03 +0545

    Updated pokemon api using classes

--- Pokemonapi.dart
+++ Pokemonapi.dart
@@ -1,25 +1,40 @@
-import 'package:http/http.dart 'as http;
+import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:async';
-const request="https://pokeapi.co/api/v2/ability/";
+class Pokemon
+{
+    int  count;   //which gives values of count 
+    String next;  // which gives url of next
+    List<Map> results;
+    Pokemon({this.count,this.next,this.results});   //Constructor
 
-Map data;
-List list;
+    factory Pokemon.fromjson(Map<String,dynamic> json)   //Use of Factory constructor
+{
+    var obj=json['results'];
+    return Pokemon(count:json['count'],next:json['next'],results:List<Map>.from(obj));
+}
+
+    
+}
+
+
+
 void main()async
 {
-  await getvalues('1');
+    await getvalues();
 }
-Future<dynamic> getvalues(String api)async
+Future<void> getvalues()async
 {
-    var url=Uri.parse(request+api);
-    var response=await http.get(url);
-    print(response.body);
-    data=json.decode(response.body);
-    list=data["names"];
-    print(list);
-    for(var i=0; i<list.length; i++)
+    var request=Uri.parse("https://pokeapi.co/api/v2/ability/");
+    var response=await http.get(request);
+    var a=Pokemon.fromjson(json.decode(response.body));  //It creates the Map<String,dynamic> object from json string.s
+    print("Count=${a.count}");
+    print("\nNext Link-${a.next}");
+    var b=a.results;   //varible b stores the list of map
+  // print("\nList of map:$b");
+    print("\nList of Pokemon names:");
+    for(var i=0; i<b.length; i++)
     {
-        print(list[i]['name']);
+        print(b[i]['name']); // It will give the name which is inside a list of map
     }
-    print("My favoutite pokemon out of these are ${list[7]['name']}");
+    
 }
